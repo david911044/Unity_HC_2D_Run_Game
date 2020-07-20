9b1f264f58bd2e4245bee4ece45d829ae3773ab4
david911044 <61416046+david911044@users.noreply.github.com>
2020-07-20 21:29:18 +0800

    finishi start c

---
+++ 2DRunGame/Assets/Scripts/car.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class car : MonoBehaviour
+    {
+    //欄位
+    
+    // 資料  
+    // 品牌 、C數 、重量 、顏色 、是否有天窗
+    // 
+    }
---
+++ 2DRunGame/Assets/Scripts/first.cs
@@ -0,0 +1,12 @@
+﻿//
+//
+using UnityEngine;
+
+//類別 class (腳本) 類別名稱
+public class first : MonoBehaviour
+{
+    //
+    //
+    //括號 
+    // ()、[] 、{} 、"" 、
+}
